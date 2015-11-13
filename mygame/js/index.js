20b98e3a69ee49c6f75fadd4e6cc6111dd77c23a
LouisJames <louis.alvernhe@gmail.com>
2015-11-13 10:17:25 +0800

    game

---
+++ mygame/js/index.js
@@ -0,0 +1,40 @@
+function game() {
+  var age=prompt("what is your age?");
+if (age <11)
+{ 
+alert ("You can not play!! Go away");
+}
+else
+{
+  alert("Continue the journey");
+}
+  
+var dog=prompt("Ants in your pants!! What do you do go to toilet and shake them out or act normally");
+if (dog === "go to toilet and shake them out")
+{ 
+alert ("Continue the journey");
+}
+else
+{
+  alert("You failed go away");
+}
+
+var pig=prompt("What if there is no toilet what do you do? wait in till the end of school or pretend your sick and go home");
+if (pig === "pretend your sick and go home")
+{ 
+alert ("You finished well done now you know how to survive with ants in your pants!!!");
+}
+else
+{
+  alert("You don't know how to survive with ants in your pants you suck!!!!");
+}
+
+  alert("If you failed go away if you haven't please stay and continue your journey");
+  
+
+   alert("Your parents notice your not sick")
+   
+alert("To be continued") 
+
+
+}
